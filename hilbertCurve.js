55d518dfe4d71186eb5910625b403c4906020dad
Alex Tsang <atsang1117@hotmail.com>
2016-04-06 19:00:44 -0700

    added hilbert curve algorithm and updated map generator

---
+++ hilbertCurve.js
@@ -0,0 +1,41 @@
+//input is x and y position of cell, returns which node it is in the graph
+//n is the dimension you are going up to with the graph
+function xy2d(n, x, y) {
+    var rx, ry, s, d = 0;
+    for (s=n/2; s>0; s/=2){
+        rx = (x & s) > 0;
+        ry = (y & s) > 0;
+        d += s * s * ((3 * rx) ^ ry);
+        rot (s, x, y, rx, ry);
+    }
+    return d;
+}
+
+//takes in the node order and returns the x and y coordinate on the graph
+//n is the dimension you are going up to wiht the graph
+function d2xy(n, d, x, y) {
+    var rx, ry, s, t = d;
+    x = y = 0;
+    for (s=1; s < n; s *= 2) {
+        rx = 1 & (t/2);
+        ry = 1 & (t ^ rx);
+        rot(s, x, y, rx, ry);
+        x += s * rx;
+        y += s * ry;
+        t /= 4;
+    }
+}
+
+//rotate the cell to fit graph of this dimension
+function rot(s, x, y, rx, ry) {
+    if (ry == 0) {
+        if (rx == 0) {
+            x = n-1 - x;
+            y = n-1 - y;
+        }
+        
+        var t = x;
+        x = y;
+        y = t;
+    }
+}
--- mapGenerator.js
+++ mapGenerator.js
@@ -1,65 +1,90 @@
-function generateMap(w, h) {
-    var width = w;
-    var height = h;
-    
+function generateMap(exit) {
+    //arbitrary 15x15 maps each node
+    var width = 15;
+    var height = 15;
+
+    var exitSide = exit;
+
     var map = new int[width, height];
-    randomFillMap(width, height, map);
-    
-    for(i=0; i<5; i++){
+    randomFillMap(width, height, map, exitSide);
+
+    for (i = 0; i < 5; i++) {
         smoothMap(map);
     }
-    
+
     return map;
 }
 
-function randomFillMap(width, height, map, random, percent){
-    var useRandomSeed = random;
+function randomFillMap(width, height, map, exitSide) {
     var seed;
-    var randomFillPercent = percent;
-    
-    if (useRandomSeed) {
-        //needs to be changed for time
-        seed = new Date().toUTCString;    
-    }
-    
-    for (x = 0; x < width; x++){
+    var randomFillPercent = 30;
+
+    //needs to be changed for time
+    seed = new Date().toUTCString;
+
+    for (x = 0; x < width; x++) {
         for (y = 0; y < height; y++) {
-            if (x == 0 || x == width-1 || y==0 || y == height - 1) {
-                map[x,y] = 1;
+            if (x == 0 || x == width - 1 || y == 0 || y == height - 1) {
+                map[x, y] = 1;
             } else {
                 // needs to be changed for using random number generator
-                map[x,y] = (Math.floor(Math.random()*100) < randomFillPercent) ? 1 : 0;
+                map[x, y] = (Math.floor(Math.random() * 100) < randomFillPercent) ? 1 : 0;
             }
         }
     }
 }
 
 function smoothMap(map) {
-    for (x=0; x < width; x++) {
-        for (y = 0;y < height; y++){
-             var neighbourWallTiles= getSurroundingWalls(x,y);
-             
-             if (neighbourWallTiles < 4) {
-                 map[x,y] = 0;
-             } else if (neighbourWallTiles > 4) {
-                 map[x,y] = 1;
-             }
+    for (x = 0; x < width; x++) {
+        for (y = 0; y < height; y++) {
+            var neighbourWallTiles = getSurroundingWalls(x, y);
+
+            if (neighbourWallTiles < 4) {
+                map[x, y] = 0;
+            } else if (neighbourWallTiles > 4) {
+                map[x, y] = 1;
+            }
         }
     }
 }
 
 function getSurroundingWalls(x, y) {
     var wallcount = 0;
-    for (neighbourX = x- 1; neightbourX <= x+1; x++){
-        for (neighbourY = y - 1; neighbourY <= y+1; y++) {
-            if (neighbourX >=0 && neighbourY >=0 && neighbourX < width && neighbourY < height) {
+    for (neighbourX = x - 1; neightbourX <= x + 1; x++) {
+        for (neighbourY = y - 1; neighbourY <= y + 1; y++) {
+            if (neighbourX >= 0 && neighbourY >= 0 && neighbourX < width && neighbourY < height) {
                 if (neighbourX != x || neighbourY != y) {
                     wallcount += map[neighbourX, neighbourY];
                 }
             } else {
-                wallcount ++;
+                wallcount++;
             }
         }
     }
     return wallcount;
 }
+
+function createExits(side, map) {
+    switch (side) {
+        case "n":
+            map[8, 0] = 0;
+            map[7, 0] = 0;
+            map[9, 0] = 0;
+            break;
+        case "w":
+            map[0, 8] = 0;
+            map[0, 7] = 0;
+            map[0, 9] = 0;
+            break;
+        case "e":
+            map[15, 8] = 0;
+            map[15, 7] = 0;
+            map[15, 9] = 0;
+            break;
+        case "s":
+            map[8, 15] = 0;
+            map[7, 15] = 0;
+            map[9, 15] = 0;
+            break;
+    }
+} 
