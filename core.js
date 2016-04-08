b670252996a4ae85cafbb497c169feacfb085880
Brandon Lim <b_loong5@hotmail.com>
2016-04-08 14:33:34 -0700

    Added collision detection with the sun.

--- core.js
+++ core.js
@@ -10,10 +10,13 @@ var mapWidth = mapHeight * aspectRatio;
 var mouseX = 0, mouseY = 0;
 var windowWidth, windowHeight;
 var fullCamera, mapCamera;
+var freezeMode = false;
 var controls;
 
 var sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune;
 var sunHeight = 500;
+var earthRadius = 1, sunRadius = 5;
+var enemyObjects = [sun];
 
 // ============================================================================
 //
@@ -153,105 +156,94 @@ function initGeometry() {
     var material = new THREE.MeshBasicMaterial();
     material.map = THREE.ImageUtils.loadTexture('assets/sunmap.jpg');
     sun = new THREE.Mesh(sunGeometry, material);
-    var sunPosition = new THREE.Matrix4().makeTranslation(0, sunHeight, 0);
+    var sunPosition = new THREE.Matrix4().makeTranslation(0, 0, 50);
     sun.applyMatrix(sunPosition);
     scene.add(sun);
 
-    // initialize mercury
-    var material = new THREE.MeshPhongMaterial();
-    var mercuryGeometry = new THREE.SphereGeometry(0.5, 32, 32);
-    generateVertexColors(mercuryGeometry);
-    material.map = THREE.ImageUtils.loadTexture('assets/mercurymap.jpg');
-    mercury = new THREE.Mesh(mercuryGeometry, material);
-    var mercuryPosition = new THREE.Matrix4().makeTranslation(10, 0, 0);
-    mercury.applyMatrix(mercuryPosition);
-    sun.add(mercury);
-    //createOrbitCircle(10);
-
-    // initialize venus
-    var material = new THREE.MeshPhongMaterial();
-    var venusGeometry = new THREE.SphereGeometry(0.75, 32, 32);
-    generateVertexColors(venusGeometry);
-    material.map = THREE.ImageUtils.loadTexture('assets/venusmap.jpg');
-    venus = new THREE.Mesh(venusGeometry, material);
-    var venusPosition = new THREE.Matrix4().makeTranslation(13, 0, 0);
-    venus.applyMatrix(venusPosition);
-    sun.add(venus);
-    //createOrbitCircle(13);
-
-    // initialize earth
+    // // initialize mercury
     // var material = new THREE.MeshPhongMaterial();
-    // material.map = THREE.ImageUtils.loadTexture('assets/earthmap1k.jpg');
-    // var earthGeometry = new THREE.SphereGeometry(1, 32, 32);
-    // generateVertexColors(earthGeometry);
-    // earth = new THREE.Mesh(earthGeometry, material);
-    // var earthPosition = new THREE.Matrix4().makeTranslation(16, 0, 0);
-    // earth.applyMatrix(earthPosition);
-    // sun.add(earth);
-    //createOrbitCircle(16);
-
-    // initialize mars
-    var material = new THREE.MeshPhongMaterial();
-    var marsGeometry = new THREE.SphereGeometry(1, 32, 32);
-    generateVertexColors(venusGeometry);
-    material.map = THREE.ImageUtils.loadTexture('assets/mars_1k_color.jpg');
-    mars = new THREE.Mesh(marsGeometry, material);
-    var marsPosition = new THREE.Matrix4().makeTranslation(19, 0, 0);
-    mars.applyMatrix(marsPosition);
-    sun.add(mars);
-    //createOrbitCircle(19);
-
-    // initialize jupiter
-    var material = new THREE.MeshPhongMaterial();
-    var jupiterGeometry = new THREE.SphereGeometry(3, 32, 32);
-    generateVertexColors(jupiterGeometry);
-    material.map = THREE.ImageUtils.loadTexture('assets/jupitermap.jpg');
-    jupiter = new THREE.Mesh(jupiterGeometry, material);
-    var jupiterPosition = new THREE.Matrix4().makeTranslation(25, 0, 0);
-    jupiter.applyMatrix(jupiterPosition);
-    sun.add(jupiter);
-    //createOrbitCircle(25);
-
-    // initialize saturn
-    var material = new THREE.MeshPhongMaterial();
-    var saturnGeometry = new THREE.SphereGeometry(2, 32, 32);
-    generateVertexColors(saturnGeometry);
-    material.map = THREE.ImageUtils.loadTexture('assets/saturnmap.jpg');
-    saturn = new THREE.Mesh(saturnGeometry, material);
-    var saturnPosition = new THREE.Matrix4().makeTranslation(35, 0, 0);
-    saturn.applyMatrix(saturnPosition);
-    //createOrbitCircle(35);
-
-    // initialize saturn rings
-    var geometry = new THREE.RingGeometry(3, 5, 32, 32);
-    var material = new THREE.MeshBasicMaterial({ side: THREE.DoubleSide });
-    material.map = THREE.ImageUtils.loadTexture('assets/saturnringcolor.jpg');
-    var saturn_rings = new THREE.Mesh(geometry, material);
-    saturn_rings.lookAt(new THREE.Vector3(0, 1, 0));
-    saturn.add(saturn_rings);
-    sun.add(saturn);
-
-    // initialize uranus
-    var material = new THREE.MeshPhongMaterial();
-    var uranusGeometry = new THREE.SphereGeometry(2, 32, 32);
-    generateVertexColors(uranusGeometry);
-    material.map = THREE.ImageUtils.loadTexture('assets/uranusmap.jpg');
-    uranus = new THREE.Mesh(uranusGeometry, material);
-    var uranusPosition = new THREE.Matrix4().makeTranslation(45, 0, 0);
-    uranus.applyMatrix(uranusPosition);
-    sun.add(uranus);
-    //createOrbitCircle(45);
-
-    // initialize neptune
-    var material = new THREE.MeshPhongMaterial();
-    var neptuneGeometry = new THREE.SphereGeometry(2, 32, 32);
-    generateVertexColors(uranusGeometry);
-    material.map = THREE.ImageUtils.loadTexture('assets/neptunemap.jpg');
-    neptune = new THREE.Mesh(neptuneGeometry, material);
-    var neptunePosition = new THREE.Matrix4().makeTranslation(55, 0, 0);
-    neptune.applyMatrix(neptunePosition);
-    sun.add(neptune);
-    //createOrbitCircle(55);
+    // var mercuryGeometry = new THREE.SphereGeometry(0.5, 32, 32);
+    // generateVertexColors(mercuryGeometry);
+    // material.map = THREE.ImageUtils.loadTexture('assets/mercurymap.jpg');
+    // mercury = new THREE.Mesh(mercuryGeometry, material);
+    // var mercuryPosition = new THREE.Matrix4().makeTranslation(10, 0, 0);
+    // mercury.applyMatrix(mercuryPosition);
+    // sun.add(mercury);
+    // //createOrbitCircle(10);
+    //
+    // // initialize venus
+    // var material = new THREE.MeshPhongMaterial();
+    // var venusGeometry = new THREE.SphereGeometry(0.75, 32, 32);
+    // generateVertexColors(venusGeometry);
+    // material.map = THREE.ImageUtils.loadTexture('assets/venusmap.jpg');
+    // venus = new THREE.Mesh(venusGeometry, material);
+    // var venusPosition = new THREE.Matrix4().makeTranslation(13, 0, 0);
+    // venus.applyMatrix(venusPosition);
+    // sun.add(venus);
+    // //createOrbitCircle(13);
+    //
+    // // initialize mars
+    // var material = new THREE.MeshPhongMaterial();
+    // var marsGeometry = new THREE.SphereGeometry(1, 32, 32);
+    // generateVertexColors(venusGeometry);
+    // material.map = THREE.ImageUtils.loadTexture('assets/mars_1k_color.jpg');
+    // mars = new THREE.Mesh(marsGeometry, material);
+    // var marsPosition = new THREE.Matrix4().makeTranslation(19, 0, 0);
+    // mars.applyMatrix(marsPosition);
+    // sun.add(mars);
+    // //createOrbitCircle(19);
+    //
+    // // initialize jupiter
+    // var material = new THREE.MeshPhongMaterial();
+    // var jupiterGeometry = new THREE.SphereGeometry(3, 32, 32);
+    // generateVertexColors(jupiterGeometry);
+    // material.map = THREE.ImageUtils.loadTexture('assets/jupitermap.jpg');
+    // jupiter = new THREE.Mesh(jupiterGeometry, material);
+    // var jupiterPosition = new THREE.Matrix4().makeTranslation(25, 0, 0);
+    // jupiter.applyMatrix(jupiterPosition);
+    // sun.add(jupiter);
+    // //createOrbitCircle(25);
+    //
+    // // initialize saturn
+    // var material = new THREE.MeshPhongMaterial();
+    // var saturnGeometry = new THREE.SphereGeometry(2, 32, 32);
+    // generateVertexColors(saturnGeometry);
+    // material.map = THREE.ImageUtils.loadTexture('assets/saturnmap.jpg');
+    // saturn = new THREE.Mesh(saturnGeometry, material);
+    // var saturnPosition = new THREE.Matrix4().makeTranslation(35, 0, 0);
+    // saturn.applyMatrix(saturnPosition);
+    // //createOrbitCircle(35);
+    //
+    // // initialize saturn rings
+    // var geometry = new THREE.RingGeometry(3, 5, 32, 32);
+    // var material = new THREE.MeshBasicMaterial({ side: THREE.DoubleSide });
+    // material.map = THREE.ImageUtils.loadTexture('assets/saturnringcolor.jpg');
+    // var saturn_rings = new THREE.Mesh(geometry, material);
+    // saturn_rings.lookAt(new THREE.Vector3(0, 1, 0));
+    // saturn.add(saturn_rings);
+    // sun.add(saturn);
+    //
+    // // initialize uranus
+    // var material = new THREE.MeshPhongMaterial();
+    // var uranusGeometry = new THREE.SphereGeometry(2, 32, 32);
+    // generateVertexColors(uranusGeometry);
+    // material.map = THREE.ImageUtils.loadTexture('assets/uranusmap.jpg');
+    // uranus = new THREE.Mesh(uranusGeometry, material);
+    // var uranusPosition = new THREE.Matrix4().makeTranslation(45, 0, 0);
+    // uranus.applyMatrix(uranusPosition);
+    // sun.add(uranus);
+    // //createOrbitCircle(45);
+    //
+    // // initialize neptune
+    // var material = new THREE.MeshPhongMaterial();
+    // var neptuneGeometry = new THREE.SphereGeometry(2, 32, 32);
+    // generateVertexColors(uranusGeometry);
+    // material.map = THREE.ImageUtils.loadTexture('assets/neptunemap.jpg');
+    // neptune = new THREE.Mesh(neptuneGeometry, material);
+    // var neptunePosition = new THREE.Matrix4().makeTranslation(55, 0, 0);
+    // neptune.applyMatrix(neptunePosition);
+    // sun.add(neptune);
+    // //createOrbitCircle(55);
 
     var FloorGeo = new THREE.BoxGeometry(150, 5, 300);
     var floormaterial = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
@@ -304,26 +296,27 @@ function createOrbitCircle(radius) {
 }
 
 function updateSystem() {
+  if (!freezeMode) {
     // orbit around sun
-    mercury.applyMatrix(new THREE.Matrix4().makeRotationY(0.04));
-    venus.applyMatrix(new THREE.Matrix4().makeRotationY(0.02));
-    // earth.applyMatrix(new THREE.Matrix4().makeRotationY(0.01));
-    mars.applyMatrix(new THREE.Matrix4().makeRotationY(0.008));
-    jupiter.applyMatrix(new THREE.Matrix4().makeRotationY(0.005));
-    saturn.applyMatrix(new THREE.Matrix4().makeRotationY(0.003));
-    uranus.applyMatrix(new THREE.Matrix4().makeRotationY(0.002));
-    neptune.applyMatrix(new THREE.Matrix4().makeRotationY(0.001));
-
-    // planet individual rotations
-    mercury.rotateOnAxis(new THREE.Vector3(0, 1, 0), 0.000853);
-    venus.rotateOnAxis(new THREE.Vector3(0, 1, 0), -0.0002057);
-    // earth.rotateOnAxis(new THREE.Vector3(0, 1, 0), 0.05);
-    mars.rotateOnAxis(new THREE.Vector3(0, 1, 0), 0.0487);
-    jupiter.rotateOnAxis(new THREE.Vector3(0, 1, 0), 0.1219);
-    saturn.rotateOnAxis(new THREE.Vector3(0, 1, 0), 0.11737);
-    uranus.rotateOnAxis(new THREE.Vector3(1, 0, 0), 0.0696);
-    neptune.rotateOnAxis(new THREE.Vector3(0, 1, 0), 0.07448);
-
+    // mercury.applyMatrix(new THREE.Matrix4().makeRotationY(0.04));
+    // venus.applyMatrix(new THREE.Matrix4().makeRotationY(0.02));
+    // // earth.applyMatrix(new THREE.Matrix4().makeRotationY(0.01));
+    // mars.applyMatrix(new THREE.Matrix4().makeRotationY(0.008));
+    // jupiter.applyMatrix(new THREE.Matrix4().makeRotationY(0.005));
+    // saturn.applyMatrix(new THREE.Matrix4().makeRotationY(0.003));
+    // uranus.applyMatrix(new THREE.Matrix4().makeRotationY(0.002));
+    // neptune.applyMatrix(new THREE.Matrix4().makeRotationY(0.001));
+    //
+    // // planet individual rotations
+    // mercury.rotateOnAxis(new THREE.Vector3(0, 1, 0), 0.000853);
+    // venus.rotateOnAxis(new THREE.Vector3(0, 1, 0), -0.0002057);
+    // // earth.rotateOnAxis(new THREE.Vector3(0, 1, 0), 0.05);
+    // mars.rotateOnAxis(new THREE.Vector3(0, 1, 0), 0.0487);
+    // jupiter.rotateOnAxis(new THREE.Vector3(0, 1, 0), 0.1219);
+    // saturn.rotateOnAxis(new THREE.Vector3(0, 1, 0), 0.11737);
+    // uranus.rotateOnAxis(new THREE.Vector3(1, 0, 0), 0.0696);
+    // neptune.rotateOnAxis(new THREE.Vector3(0, 1, 0), 0.07448);
+  }
 }
 
 // SETUP UPDATE CALL-BACK
@@ -388,10 +381,28 @@ function onKeyDown(event) {
     if (keyboard.eventMatches(event, "w")) {
         // move camera forwards
         earth.translateOnAxis(new THREE.Vector3(0,0,-1), 1);
+
+        // enemy collision detection. Relatively simple, we don't need it to be
+        // super exact for the sun;
+        var earthPosition = new THREE.Vector3(earth.position.x, earth.position.y, earth.position.z);
+        var sunPosition = new THREE.Vector3(sun.position.x, sun.position.y, sun.position.z);
+
+        if (earthPosition.distanceTo(sunPosition) <= earthRadius + sunRadius) {
+          beginBossFight();
+        }
     }
     else if (keyboard.eventMatches(event, "s")) {
         // move camera back
         earth.translateOnAxis(new THREE.Vector3(0,0,-1), -1);
+
+        // enemy collision detection. Relatively simple, we don't need it to be
+        // super exact for the sun;
+        var earthPosition = new THREE.Vector3(earth.position.x, earth.position.y, earth.position.z);
+        var sunPosition = new THREE.Vector3(sun.position.x, sun.position.y, sun.position.z);
+
+        if (earthPosition.distanceTo(sunPosition) <= earthRadius + sunRadius) {
+          beginBossFight();
+        }
     }
     else if (keyboard.eventMatches(event, "a")) {
         // rotate camera left
